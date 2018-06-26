dde1b77d232d26dc5c896a58d001dcfa726b3799
Lily Carlson <l.hamelaswood@gmail.com>
2018-06-26 16:04:50 +0000

    Added ability to search repos with github api

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ class SearchForm extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			value: ''
+			value: '',
+			results: [],
+			loading: false,
 		};
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
@@ -17,12 +19,32 @@ class SearchForm extends React.Component {
 	}
 
 	handleSubmit(event){
-		alert('A search was submitted: ' + this.state.value);
+		this.setState({
+			loading: true
+		})
+		this.performSearch(this.state.value);
 		event.preventDefault();
 	}
 
+	performSearch(query) {
+	    //&sort=stars&order=desc`)
+	    fetch(`https://api.github.com/search/repositories?q=${query}`)
+	      .then(response => response.json())
+	      .then(responseData => {
+	      	console.log(responseData.items);
+	        this.setState({
+	          results: responseData.items,
+	          loading: false
+	        });
+	      })
+	      .catch(error => {
+	        console.log('Error fetching and parsing data', error);
+	      });
+  	}
+
 	render(){
 		return (
+			<div>
 			<form onSubmit={this.handleSubmit}>
 				<label> 
 					Search:
@@ -34,14 +56,35 @@ class SearchForm extends React.Component {
 				</label>
 				<input type="submit" value="Submit"/>
 			</form>
-
+				<RepoList repos={this.state.results}/>
+			</div>
 
 		);
 	}
 }
 
+function RepoItem(props) {
+  return <li>{props.value}</li>;
+}
+
+function RepoList(props) {
+	if (props.repos){
+		return (
+		  <ul>
+		    {props.repos.map((repo) =>
+		      <RepoItem key={repo.id}
+		                value={repo.name} />
+		    )}
+		  </ul>
+		);  
+	} 
+	else {
+		return null;
+	}	
+}
+
 ReactDOM.render(
-  <SearchForm />,
+  < SearchForm />,
   document.getElementById('root')
 
 );
